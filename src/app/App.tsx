4061f4cf1ce6da4cbe42d5e94cdfd01d961d334f
JDonn319 <j3137514@gmail.com>
2026-09-21 16:28:49 +0300

    Create App.tsx

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  const containerStyle: React.CSSProperties = {
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: '#000000',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+  return <main style={containerStyle} />;
+}
